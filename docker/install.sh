5573b740012d316ad4547f1aa11aeca83d20bf1d
Jon Jagger <jon@jaggersoft.com>
2020-01-18 18:44:38 +0000

    Switch to circleci

---
+++ docker/install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -Eeu
+
+apt-get update
+apt-get install --yes git
+
+cd /usr/src
+git clone https://github.com/joakimkarlsson/igloo.git
+cd igloo
+git checkout -b cyberdojorelease igloo.1.1.1
+git submodule init
+git submodule update
+
+mkdir -p /usr/include/igloo
+cp -rfv /usr/src/igloo/igloo/ /usr/include
+
+cd ..
+rm -rf igloo
+
+apt-get remove --yes git
